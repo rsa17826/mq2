fe1df630ec007d0214746825cefc3de7cdcb1755
nyix <rsa17826@email.vccs.edu>
2026-08-07 15:08:51 -0400

    NO MESSAGE SET

--- cgi-bin/saveChar.py
+++ cgi-bin/saveChar.py
@@ -7,11 +7,13 @@ reg = re.compile("&stat\\d+=([^&]+)")
 print("Content-Type: text/plain")
 print() # blank line is REQUIRED
 try:
-  with open(
-    os.path.join(os.path.dirname(__file__), "../MQ2Files/loadChar2.php"), "w"
-  ) as file:
-    newstr = re.findall(reg, os.environ.get("QUERY_STRING", ""))
-    file.write(" ".join(newstr))
-    print("go", end="")
+    with open(
+        os.path.join(os.path.dirname(__file__), "../MQ2Files/loadChar2.php"), "w"
+    ) as file:
+        newstr = re.findall(reg, os.environ.get("QUERY_STRING", ""))
+        file.write(" ".join(newstr))
+        print("go", end="")
+
+
 except Exception as e:
-  print("stop", end="")
+    print("stop", end="")
